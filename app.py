a43c2fff30e7fb1fa1676ddb9b4a560d4b0a8985
paulorss <61366907+paulorss@users.noreply.github.com>
2020-07-13 13:55:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 from flask import Flask, render_template, request
-#from bs4 import NavigableString
-#from bs4 import BeautifulSoup
-#from easygui import *
-#import requests
-#import re
-#import nltk
-#import csv
-#import sys
-#import easygui as eg
 from newspaper import Article
 from googlesearch import search
-
 
 app = Flask(__name__)
 
@@ -26,9 +16,7 @@ def get_bot_response():
     listag = []
     for urla in search(rec, tld='com.br', lang='pt-br', stop=3):
         listag.append(urla)
-    else:
-        print('Pesquisa não encontrada! Poderia ser mais específico, por favor!')
-
+    
     g = int(len(listag))
     print(g)
 
@@ -36,9 +24,7 @@ def get_bot_response():
     for z in range(0, g):
         ur = str(listag[z])
         listago.append(ur)
-    else:
-        print('Pesquisa não encontrada! Poderia ser mais específico, por favor!')
-
+    
     print(listago)
     qo = int(len(listago))
 
@@ -49,13 +35,11 @@ def get_bot_response():
         article.download()
         article.parse()
         article.text
-        article.nlp()
-        article.summary
         reports2.append(str(article.text).replace('\n', ''))
 
     resposta_final = str(reports2).replace('\n', ' ')
-    return str(resposta_final) or ("PESQUISA NÃO ENCONTRADA!")
-
-
+       
+    return str(resposta_final) 
+  
 if __name__ == "__main__":
     app.run()
